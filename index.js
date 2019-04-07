b708d8dc7b21a61f5874c1f6158d83cdf5b9a200
Tasfia <tsultana5226@bths.edu>
2019-04-07 14:49:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+function questionOne(){
+    var one = document.getElementById("one").value;
+    if (one == "d" || one == "D" || one == "interviewed the top three contestants") {
+        document.getElementById("answer1").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer1").innerHTML = one + " is incorrect.";
+    }
+}
+
+function questionTwo(){
+    var two = document.getElementById("two").value;
+    if (two == "b" || two == "B" || two == "a summer of strange weather and its probable cause"){
+        document.getElementById("answer2").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer2").innerHTML = two + " is incorrect.";
+    }
+}
+
+function questionThree(){
+    var three = document.getElementById("three").value;
+    if (three == "a" || tbree == "A" || three == "They believed that the cold weather could not continue all summer long." ){
+        document.getElementById("answer3").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer3").innerHTML = three + " is incorrect.";
+    }
+}
+
+function questionFour(){
+    var four = document.getElementById("four").value;
+    if (four == "c" || four == "C" || four == "shortages of fruits, vegetables, and other essential crops."){
+        document.getElementById("answer4").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer4").innerHTML = four + " is incorrect.";
+    }
+}
+
+function questionFive(){
+    var five = document.getElementById("five").value;
+    if (five == "b" || five == "B" || five == "It shows how nineteenth-century people explained the 1816 weather."){
+        document.getElementById("answer5").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer5").innerHTML = five + " is incorrect.";
+    }
+}
+
+function questionSix(){
+    var six = document.getElementById("six").value;
+    if (six == "d" || six == "D" || six == "11800"){
+        document.getElementById("answer6").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer6").innerHTML = six + " is incorrect.";
+    }
+}
+
+function questionSeven(){
+    var seven = document.getElementById("seven").value;
+    if (seven == "b" || seven == "B" || seven == "15" ){
+        document.getElementById("answer7").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer7").innerHTML = seven + " is incorrect.";
+    }
+}
+
+function questionEight(){
+    var eight = document.getElementById("eight").value;
+    if (eight == "c" || eight == "C" || eight == "33"){
+        document.getElementById("answer8").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer8").innerHTML = eight + " is incorrect.";
+    }
+}
+
+function questionNine(){
+    var nine = document.getElementById("nine").value;
+    if (nine == "a" || nine == "A" || nine == "0"){
+        document.getElementById("answer9").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer9").innerHTML = nine + " is incorrect.";
+    }
+}
+
+function questionTen(){
+    var ten = document.getElementById("ten").value;
+    if (ten == "b" || ten == "B" || ten == "16%" || ten == "16"){
+        document.getElementById("answer10").innerHTML = "Correct answer.";
+    }else{
+        document.getElementById("answer10").innerHTML = ten + " is incorrect.";
+    }
+}
